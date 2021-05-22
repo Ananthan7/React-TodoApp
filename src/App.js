44457d66d5330d5dc388e06a4aa50004ed45bfa7
Ananthan7 <ananthankrishnan77@gmail.com>
2021-05-22 15:38:39 +0530

    updated components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const date=new Date();
+  // state
+  const [todos,setTodos] = useState([]);
+  const [todo,setTodo] = useState('');
+
+  const todoData = (e) => {
+    setTodo(e.target.value);
+    e.preventDefault()
+  }
+  const storeTodo =()=>{
+    setTodos([...todos,todo]);
+  }
+  const deleteTodo= key =>{
+    setTodos(()=>todos.filter((data,index)=>index!==key))
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Helooo... its {date.getDate()} /{date.getMonth()}</h2>
+      </div>
+      <div className="input">
+        <input value={todo} onChange={todoData} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={storeTodo} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        { todos.map((data,index) =>{
+          return( 
+            <div className="todo">
+              <div className="left">
+                <input type="checkbox" name="" id="" />
+                <p key={index}>{data}</p>
+              </div>
+              <div className="right">
+                <i onClick={()=>deleteTodo(index)} className="fas fa-times"></i>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
